798224820e3619f765cf428e340f55d252c27bf4
Phodal Huang <h@phodal.com>
2022-08-05 23:16:41 +0800

    build: add env for config

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
+  const env = process.env;
   return (
     <div>
       <h1>Layout's Todo</h1>
+      <p>{JSON.stringify(env)}</p>
       <nav>
         <Link to="/">Home</Link> |{" "}
         <Link to="/about">About</Link>
