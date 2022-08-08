e4a4ef27c6680c7a3309da9a10ef4d723a5c2675
Phodal Huang <h@phodal.com>
2022-08-08 12:54:42 +0800

    build: update ci scripts

---
+++ serve.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+let server;
+const app = express();
+app.use(express.static(path.join(__dirname, './build')));
+
+server = new http.createServer(app);
+
+server.on('error', err => console.log('Server error:', err));
+server.listen(process.env.PORT);
