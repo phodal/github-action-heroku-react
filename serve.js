b8089a9e2d9a48d3209a3fb48ce42c1b9ab8b0b5
Phodal Huang <h@phodal.com>
2022-08-08 20:06:51 +0800

    build: add log env for backend debug

--- serve.js
+++ serve.js
@@ -9,4 +9,5 @@ app.use(express.static(path.join(__dirname, './build')));
 server = new http.createServer(app);
 
 server.on('error', err => console.log('Server error:', err));
+console.log(process.env);
 server.listen(process.env.PORT);
